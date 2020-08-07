70373a93e978044312440c9f8340804698cc01a6
Matheus Reis <matheus.reis@olist.com>
2020-08-07 12:10:16 -0300

    product condition model

---
+++ app/model/product_condition.py
@@ -0,0 +1,24 @@
+from app.model.base_model import BaseModel
+
+class ProductCondition(BaseModel):
+
+    def __init__(self, name: str, description: str, id: int = 0):
+        self.__name = name
+        self.__description = description
+        super().__init__(id = id)
+
+    @property
+    def name(self) -> str:
+        return self.__name
+    
+    @name.setter
+    def name(self, name:str):
+        self.__name = name
+
+    @property
+    def description(self) -> str:
+        return self.__description
+    
+    @description.setter
+    def description(self, description:str):
+        self.__description = description
