f4ee73208eaec65fe7e5e98cb4a25dc2908761e6
Paulo Netto <pauloestrella1994@gmail.com>
2020-08-10 11:06:32 -0300

    Fixing formats for typing hints

--- app/model/product_rating.py
+++ app/model/product_rating.py
@@ -2,7 +2,7 @@ from app.model.base_model import BaseModel
 
 class ProductRating(BaseModel):
 
-    def __init__(self, score: str = '', status: str = '', person_id: str = '', product_id: str = '', id: int = 0):
+    def __init__(self, score: str = '', status: str = '', person_id: int = 0, product_id: int = 0, id: int = 0):
         self.__score = score
         self.__status = status
         self.__person_id = person_id
@@ -26,19 +26,19 @@ class ProductRating(BaseModel):
         self.__status = status
     
     @property
-    def person_id(self) -> str:
+    def person_id(self) -> int:
         return self.__person_id
 
     @person_id.setter
-    def person_id(self, person_id:str):
+    def person_id(self, person_id:int):
         self.__person_id = person_id
     
     @property
-    def product_id(self) -> str:
+    def product_id(self) -> int:
         return self.__product_id
 
     @product_id.setter
-    def product_id(self, product_id:str):
+    def product_id(self, product_id:int):
         self.__product_id = product_id
     
     def __str__(self):
--- app/model/shipping_country.py
+++ app/model/shipping_country.py
@@ -2,7 +2,7 @@ from app.model.base_model import BaseModel
 
 class ShippingCountry(BaseModel):
 
-    def __init__(self, name: str = '', imported: str = '', id: int = 0):
+    def __init__(self, name: str = '', imported: bool = 0, id: int = 0):
         self.__name = name
         self.__imported = imported
         super().__init__(id)
@@ -16,11 +16,11 @@ class ShippingCountry(BaseModel):
         self.__name = name
 
     @property
-    def imported(self) -> str:
+    def imported(self) -> bool:
         return self.__imported
 
     @imported.setter
-    def imported(self, imported:str):
+    def imported(self, imported:bool):
         self.__imported = imported
 
     def __str__(self):
