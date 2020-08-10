3b7c64e2fa28acca0e78765166ba27d1282614cf
Matheus Reis <matheus.reis@olist.com>
2020-08-10 09:48:57 -0300

    Product category route returning json

--- app/model/base_model.py
+++ app/model/base_model.py
@@ -1,11 +1,11 @@
 class BaseModel:
-    def __init__(self, id:int = 0):
+    def __init__(self, id: int = 0):
         self.__id = id
 
     @property
-    def id(self)->int:
+    def id(self) -> int:
         return self.__id
 
     @id.setter
-    def id(self, id:int)->None:
-        self.__id = id
+    def id(self, id: int) -> None:
+        self.__id = int(id)
--- app/start.py
+++ app/start.py
@@ -1,14 +1,21 @@
 from flask import Flask
 
 from app.dao.product_dao import ProductDao
-
+from app.dao.product_category_dao import ProductCategoryDao
 
 app = Flask(__name__)
 pd = ProductDao()
+pcd = ProductCategoryDao()
+
 
 @app.route('/')
 def initial():
     return pd.read()[0].__dict__()
 
-app.run(debug=True)
 
+@app.route('/product-category/')
+def product_category():
+    return pcd.read()[0].__dict__()
+
+
+app.run(debug=True)
