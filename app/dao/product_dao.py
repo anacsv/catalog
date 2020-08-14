6c5ba2d2220de340d6ff38103f0308e9e02c3e5b
anacsv <anapaula.carneiro@olist.com>
2020-08-14 10:02:34 -0300

    Adjusted product and brand

--- app/dao/product_dao.py
+++ app/dao/product_dao.py
@@ -23,25 +23,3 @@ class ProductDao(BaseDao):
     # delete
     def delete(self, id: int) -> dict:
         return super().delete(id)
-
-    def __convert_data_object(self, data):
-        if type(data) == list:
-            products = []
-            for item in data:
-                product = self.__obj_converter(item)
-                products.append(product)
-            return products
-        product = self.__obj_converter(data)
-        return product
-
-    def __obj_converter(self, item_tuple:tuple) -> Product:
-        model = Product()
-        model.id = item_tuple[0]
-        model.name = item_tuple[1]
-        model.description = item_tuple[2]
-        model.price = item_tuple[3]
-        model.gtin = item_tuple[4]
-        model.product_condition_id = item_tuple[5]
-        model.brand_id = item_tuple[6]
-        model.shipping_country_id = item_tuple[7]
-        return model
